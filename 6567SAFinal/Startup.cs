1389777a16a0b24b5a5245f661caded1d163bf2a
AlwinKettles <alwinkettles@gmail.com>
2021-09-15 10:37:21 +0200

    Add project files.

---
+++ 6567SAFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_6567SAFinal.Startup))]
+namespace _6567SAFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
